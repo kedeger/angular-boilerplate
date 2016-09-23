1588a863c6d3bb08ed5a8ec905e73d0fa78c2190
kedeger <kedeger@uw.edu>
2016-09-23 15:19:34 -0700

    basic angular starter template created

---
+++ app.js
@@ -0,0 +1,17 @@
+(function() {
+  angular.module('App', ['ui.router'])
+  .config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    '$locationProvider',
+    function($stateProvider, $urlRouterProvider, $locationProvider) {
+      $urlRouterProvider.otherwise('/');
+
+      $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: './pages/home/home.html',
+        controller: 'HomeCtrl as vm'
+      });
+    }])
+})()
